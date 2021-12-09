6b5248fe28a80e432c8608a91232417b2a0dc073
Rodrigo Espinosa de los Monteros <1084688+RodEsp@users.noreply.github.com>
2021-12-09 09:44:07 -0500

    feat: add support for commands separated by spaces (WIP)

--- src/commands/autocomplete/create.ts
+++ src/commands/autocomplete/create.ts
@@ -109,10 +109,12 @@ compinit;\n`
 
     plugins.forEach(p => {
       p.commands.forEach(c => {
+        console.log(c)
         try {
           if (c.hidden) return
           cmds.push({
             id: c.id,
+            // id: c.id.replace(':', this.config.topicSeparator),
             description: sanitizeDescription(c.description || ''),
             flags: c.flags,
           })
@@ -184,9 +186,91 @@ compinit;\n`
 
   private get bashCompletionFunction(): string {
     const cliBin = this.cliBin
+    let bashScript = ''
 
-    return `#!/usr/bin/env bash
+    if (this.config.topicSeparator === ' ') {
+      bashScript = `#!/usr/bin/env bash
 
+# This function joins an array using a character passed in
+# e.g. ARRAY=(one two three) -> join_by ":" \${ARRAY[@]} -> "one:two:three"
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
+_${cliBin}()
+{
+
+  local cur="\${COMP_WORDS[COMP_CWORD]}" opts normalizedCommand colonPrefix IFS=$' \t\n'
+  COMPREPLY=()
+
+  local commands="
+${this.bashCommandsWithFlagsList}
+"
+
+  function __trim_colon_commands()
+  {
+    # Turn $commands into an array
+    commands=("\${commands[@]}")
+
+    if [[ -z "$colonPrefix" ]]; then
+      colonPrefix="$normalizedCommand:"
+    fi
+
+    # Remove colon-word prefix from $commands
+    commands=( "\${commands[@]/$colonPrefix}" )
+
+    for i in \${!commands[@]}; do
+      if [[ "\${commands[$i]}" == "$normalizedCommand" ]]; then
+        # If the currently typed in command is a topic command we need to remove it to avoid suggesting it again
+        unset commands[$i]
+      else
+        # Trim subcommands from each command
+        commands[$i]="\${commands[$i]%%:*}"
+      fi
+    done
+  }
+
+  if [[ "$cur" != "-"* ]]; then
+    # Command
+    __COMP_WORDS=( "\${COMP_WORDS[@]:1}" )
+
+    # The command typed by the user but separated by colons (e.g. "mycli command subcom" -> "command:subcom")
+    normalizedCommand="$( printf "%s" "$(join_by ":" \${__COMP_WORDS[@]})" )"
+
+    # The command hirarchy, with colons, leading up to the last subcommand entered (e.g. "mycli com subcommand subsubcom" -> "com:subcommand:")
+    colonPrefix="\${normalizedCommand%\${normalizedCommand##*:}}"
+
+    if [[ -z "$normalizedCommand" ]]; then
+      # If there is no normalizedCommand yet the user hasn't typed in a full command
+      # So we should trim all subcommands & flags from $commands so we can suggest all top level commands
+      opts=$(echo "$commands" | grep -Eo '^[a-zA-Z0-9_-]+')
+    else
+      # Filter $commands to just the ones that match the $normalizedCommand and turn into an array
+      commands=( $(compgen -W "$commands" -- "\${normalizedCommand}") )
+
+      # Trim higher level and subcommands from the subcommands to suggest
+      __trim_colon_commands "$colonPrefix"
+
+      opts=$(printf "%s " "\${commands[@]}") # | grep -Eo '^[a-zA-Z0-9_-]+'
+    fi
+  else 
+    # Flag
+
+    # The full CLI command separated by colons (e.g. "mycli command subcommand --fl" -> "command:subcommand")
+    # This needs to be defined with $COMP_CWORD-1 as opposed to above because the current character on the command line is "-" and we want everything before that
+    normalizedCommand="$( printf "%s" "$(join_by ":" \${COMP_WORDS[@]:1:($COMP_CWORD - 1)})" )"
+
+    # The line below finds the command in $commands using grep
+    # Then, using sed, it removes everything from the found command before the --flags (e.g. "command:subcommand:subsubcom --flag1 --flag2" -> "--flag1 --flag2")
+    opts=$(printf "$commands" | grep "\${normalizedCommand}" | sed -n "s/^\${normalizedCommand} //p")
+  fi
+
+  COMPREPLY=($(compgen -W "$opts" -- \${cur}))
+}
+
+_${cliBin} ${cliBin}
+
+`
+    } else {
+      bashScript = `#!/usr/bin/env bash
 _${cliBin}()
 {
 
@@ -219,6 +303,9 @@ ${this.bashCommandsWithFlagsList}
 
 complete -o default -F _${cliBin} ${cliBin}
 `
+    }
+
+    return bashScript
   }
 
   private get zshCompletionFunction(): string {
