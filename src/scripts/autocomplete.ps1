705d7db00626304ac30ad3b1824c88fc8dcd85b8
Rodrigo Espinosa de los Monteros <1084688+RodEsp@users.noreply.github.com>
2021-12-20 12:56:42 -0500

    fix: whoops, did not mean to include that file

--- src/scripts/autocomplete.ps1
+++
@@ -1,9 +0,0 @@
-# PowerShell parameter completion shim for the sf CLI
-Register-ArgumentCompleter -Native -CommandName sf -ScriptBlock {
-	param($commandName, $parameterName, $wordToComplete, $cursorPosition)
-		sf complete --position $cursorPosition "$wordToComplete" | 
-		
-		ForEach-Object {
-		   [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-		}
-}
